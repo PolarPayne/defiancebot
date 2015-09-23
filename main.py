43bab5b28c5a9dfb948ef864a52ead6998f24f10
Maximilian Remming <maximilian.remming@cs.helsinki.fi>
2015-09-23 16:45:34 +0300

    Removed stuff

--- main.py
+++ main.py
@@ -35,12 +35,6 @@ def main():
     if len(sys.argv) != 4:
         print("Usage: defiancebot.py <server[:port]> <channel> <nickname>")
         sys.exit(1)
-    
-    s = sys.argv[1].split(":", 1)
-    server = s[0]
-    if len(s) == 2:
-        try:
-            port = 
 
 if __name__ == "__main__":
     main()
