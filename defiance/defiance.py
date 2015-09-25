002e50b36391289a4e3be2912c3f64a82858f06d
Maximilian Remming <maximilian.remming@cs.helsinki.fi>
2015-09-26 02:41:59 +0300

    Made players into a dictionary, and fixed a few stuff

--- defiance/defiance.py
+++ defiance/defiance.py
@@ -9,26 +9,6 @@ class States(Enum):
     TEAM_SELECTION = 1
     TEAM_VOTE = 2
 
-class Player:
-    def __init__(self, nick, spy=False, role=None):
-        self.nick = nick
-        self.spy = spy
-        self.role = role
-
-    def __eq__(self, other):
-        if type(self) != type(other):
-            return False
-        return self.nick == other.nick
-
-    def __hash__(self):
-        return hash(self.nick)
-
-    def __str__(self):
-        return "nick=" + self.nick + ";spy=" + str(self.spy)
-
-    def __repr__(self):
-        return self.__str__()
-
 spies = {5:2, 6:2, 7:3, 8:3, 9:3, 10:4}
 missions = [{5:2, 6:2, 7:2, 8:3, 9:3, 10:3},
             {5:3, 6:3, 7:3, 8:4, 9:4, 10:4},
@@ -38,28 +18,27 @@ missions = [{5:2, 6:2, 7:2, 8:3, 9:3, 10:3},
 
 class Defiance:
     def __init__(self):
-        self.players = []
+        self.players = {}
         self.state = States.NOT_STARTED
         self.leader = None
         self.team = []
         self.votes = {}
+        self.round_number = 0
+        self.player_places = []
 
     def add_player(self, nick):
         if self.state is not States.NOT_STARTED:
             raise RuleException("Wrong state.")
-        for i in self.players:
-            if i.nick == nick:
-                raise RuleException("Player {} already in game.".format(nick))
-        self.players.append(Player(nick))
+        if nick in self.players:
+            raise RuleException("{} is already in the game.".format(nick))
+        self.players[nick] = False
         
     def remove_player(self, nick):
         if self.state is not States.NOT_STARTED:
             raise RuleException("Wrong state.")
-        for i in self.players:
-            if i.nick == nick:
-                del self.players[i]
-                return True
-        return False
+        if nick not in self.players:
+            raise RuleException("{} is not in the game, and cannot be removed.".format(nick))
+        del self.players[nick]
 
     def start(self):
         if len(self.players) < 5:
@@ -67,11 +46,12 @@ class Defiance:
         if len(self.players) > 10:
             raise RuleException("Too many players (max 10).")
 
-        random.shuffle(self.players)
-        for i in range(spies[len(self.players)]):
-            self.players[i].spy = True
-        random.shuffle(self.players)
-        self.leader = self.players[0]
+        self.player_places = list(self.players.keys())
+        random.shuffle(self.player_places)
+        for i in range(spies[len(self.player_places)]):
+            self.players[self.player_places[i]] = True
+        random.shuffle(self.player_places)
+        self.leader = self.player_places[0]
         self.state = States.TEAM_SELECTION
     
     def select_team(self, leader, team):
@@ -79,10 +59,12 @@ class Defiance:
             raise RuleException("Wrong state.")
         if self.leader != leader:
             raise RuleException("{}.nick is not the leader.".format(leader))
-        if len(team) != len(list(set(team2))):
+        if len(team) != len(list(set(team))):
             raise RuleException("All team members must be different.")
-        if len(team) != missions[self.state[0]][len(self.players)]:
+        if len(team) != missions[self.round_number][len(self.players)]:
             raise RuleException("Wrong number of team members.")
+        if not set(team).issubset(set(self.players.keys())):
+            raise RuleException("Some team members are not in this game")
         self.team = team
         self.state = States.TEAM_VOTE
 
@@ -115,9 +97,5 @@ if __name__ == "__main__":
     players = ["payne", "coolness", "zno", "delma", "voxwave", "harrowed", "serdion"]
     for i in players:
         game.add_player(i)
-
-    print(game.players)
-    game.start()
-    print(game.players)
-    
-    game.select_team(game.leader.nick, [])
+    game.start()    
+    game.select_team(game.leader, ["töttöröö", "kissa"])
