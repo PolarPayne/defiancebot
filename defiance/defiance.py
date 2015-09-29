e45fb082eb8b68735311b8263ff462451a89ad44
Maximilian Remming <maximilian.remming@cs.helsinki.fi>
2015-09-29 22:47:24 +0300

    Work on missions and voting

--- defiance/defiance.py
+++ defiance/defiance.py
@@ -20,22 +20,38 @@ class States(Enum):
     NOT_STARTED = 0
     TEAM_SELECTION = 1
     TEAM_VOTE = 2
-    MISSION = 3
+    ON_MISSION = 3
     SPY_VICTORY = 10
     RESISTANCE_VICTORY = 11
 
 
-spies = {5: 2, 6: 2, 7: 3, 8: 3, 9: 3, 10: 4}
-missions = [{5: 2, 6: 2, 7: 2, 8: 3, 9: 3, 10: 3},
-            {5: 3, 6: 3, 7: 3, 8: 4, 9: 4, 10: 4},
-            {5: 2, 6: 4, 7: 3, 8: 4, 9: 4, 10: 4},
-            {5: 3, 6: 3, 7: 4, 8: 5, 9: 5, 10: 5},
-            {5: 3, 6: 4, 7: 4, 8: 5, 9: 5, 10: 5}]
-double_fail = [{5: False, 6: False, 7: False, 8: False, 9: False, 10: False},
-               {5: False, 6: False, 7: False, 8: False, 9: False, 10: False},
-               {5: False, 6: False, 7: False, 8: False, 9: False, 10: False},
-               {5: False, 6: False, 7: True, 8: True, 9: True, 10: True},
-               {5: False, 6: False, 7: False, 8: False, 9: False, 10: False}]
+_spies = {5: 2, 6: 2, 7: 3, 8: 3, 9: 3, 10: 4}
+
+
+def spies_in_game(mission):
+    return _spies[mission]
+
+
+_missions = [{5: 2, 6: 2, 7: 2, 8: 3, 9: 3, 10: 3},
+             {5: 3, 6: 3, 7: 3, 8: 4, 9: 4, 10: 4},
+             {5: 2, 6: 4, 7: 3, 8: 4, 9: 4, 10: 4},
+             {5: 3, 6: 3, 7: 4, 8: 5, 9: 5, 10: 5},
+             {5: 3, 6: 4, 7: 4, 8: 5, 9: 5, 10: 5}]
+
+
+def players_in_mission(mission, players):
+    return _missions[mission][players]
+
+
+_double_fail = [{5: False, 6: False, 7: False, 8: False, 9: False, 10: False},
+                {5: False, 6: False, 7: False, 8: False, 9: False, 10: False},
+                {5: False, 6: False, 7: False, 8: False, 9: False, 10: False},
+                {5: False, 6: False, 7: True, 8: True, 9: True, 10: True},
+                {5: False, 6: False, 7: False, 8: False, 9: False, 10: False}]
+
+
+def is_double_fail_mission(mission, players):
+    return _double_fail[mission][players]
 
 
 class Player():
@@ -72,10 +88,10 @@ class Table:
             raise RuleException("{} is not in this table.".format(nick))
         self.players.remove(nick)
 
-    def select_spies(self, mission):
+    def select_spies(self):
         if self.spies_selected:
             raise RuntimeError("Spies were already selected.")
-        for i in random.sample(self.players, missions[mission][len(self.players)]):
+        for i in random.sample(self.players, spies_in_game(len(self.players))):
             i.spy = True
         self.spies_selected = True
 
@@ -95,21 +111,72 @@ class Table:
         return len(self.players)
 
 
-class Mission:
+class Votes:
     def __init__(self):
         pass
+
+    def add(self, player, vote):
+        pass
+
+    def passed(self):
+        pass
+
+
+class Mission:
+    def __init__(self):
+        self.vote = 0
+        self.votes = []
+        for i in range(5):
+            self.votes.append(Votes())
+
+    def vote(self, player, vote):
+        self.current_vote().add(player, vote)
+
+    def current_vote(self):
+        return self.votes[self.vote]
 
 
 class Board:
-    def __init__(self):
-        pass
+    def __init__(self, table):
+        self.table = table
+        self.mission = 0
+        self.missions = []
+        for i in range(5):
+            self.missions.append(Mission(players_in_mission(i, len(self.table))))
+
+    def select_team(self, team):
+        if len(team) != len(list(set(team))):
+            raise RuleException("All team members must be different.")
+        if len(team) != players_in_mission(self.mission, len(self.table)):
+            raise RuleException(
+                "Wrong number of team members, should be {}.".format(players_in_mission(self.mission, len(self.table))))
+        if not set(team).issubset(set(self.table.players)):
+            raise RuleException("Some team members are not in this game")
+
+        self.missions[self.mission] = Mission(self.table.leader, team)
+
+    def current_mission(self):
+        return self.missions[self.mission]
+
+    def vote_team(self, voter, vote):
+        self.current_mission().vote(voter, vote)
+
+    def end_team_vote(self):
+        if self.current_mission().vote_track > 5:
+            return States.SPY_VICTORY
+        elif self.mission_approved():
+            self.mission += 1
+            return States.ON_MISSION
+        else:
+            self.current_mission().vote_track += 1
+            return States.TEAM_SELECTION
 
 
 class Defiance:
     def __init__(self):
         self.state = States.NOT_STARTED
         self.table = Table()
-        self.game_state = Board()
+        self.board = Board(self.table)
 
     def check_state(self, expected):
         if self.state is not expected:
@@ -137,56 +204,28 @@ class Defiance:
     def select_team(self, leader, team):
         self.check_state(States.TEAM_SELECTION)
 
-        self.teams.add(self.table, team)
+        if self.table.leader != leader:
+            raise RuleException("Only the leader can choose a team.")
 
-        if self.state is not States.TEAM_SELECTION:
-            raise RuleException("Wrong state.")
-        if self.leader != leader:
-            raise RuleException("{}.nick is not the leader.".format(leader))
-        if len(team) != len(list(set(team))):
-            raise RuleException("All team members must be different.")
-        if len(team) != missions[self.round_number][len(self.players)]:
-            raise RuleException("Wrong number of team members.")
-        if not set(team).issubset(set(self.players.keys())):
-            raise RuleException("Some team members are not in this game")
-        self.team = team
+        self.board.select_team(team)
+
         self.state = States.TEAM_VOTE
 
     def team_vote(self, nick, vote):
-        if self.state is not States.TEAM_VOTE:
-            raise RuleException("Wrong state.")
+        self.check_state(States.TEAM_VOTE)
+
         if nick not in self.players:
             raise RuleException("{} is not a player in this game.".format(nick))
-        self.votes[nick] = vote
+
+        self.board.team_vote(nick, vote)
 
     def end_team_vote(self):
-        if self.state is not States.TEAM_VOTE:
-            raise RuleException("Wrong state.")
-        s = 0
-        for i in self.votes:
-            if i:
-                s += 1
-        # All players who didn't voted, voted approve
-        s += len(self.players) - len(self.votes)
+        self.check_state(States.TEAM_VOTE)
 
-        if self.vote_tracker > 5:
-            self.state = States.SPY_VICTORY
-
-        if s > len(self.players) // 2:
-            self.state = States.MISSION
-            self.vote_tracker = 0
-            self.votes = {}
-            return True
-        else:
-            self.votes = {}
-            self.team = None
-            self.vote_tracker += 1
-            self.state = States.TEAM_SELECTION
-            return False
+        self.state = self.board.end_team_vote()
 
     def play_mission(self, nick, play):
-        """False is failure, True is success."""
-        if self.state is not States.MISSION:
+        if self.state is not States.ON_MISSION:
             raise RuleException("Wrong state.")
         if nick not in self.players:
             raise RuleException("{} not in game.")
@@ -197,11 +236,11 @@ class Defiance:
         self.votes[nick] = play
 
     def end_mission(self):
-        if self.state is not States.MISSION:
+        if self.state is not States.ON_MISSION:
             raise RuleException("Wrong state.")
         c = Counter(self.votes.values())
         if (not double_fail[self.current_mission][len(self.players)] and c[False] > 0) or (
-                double_fail[self.current_mission][len(self.players)] and c[False] > 1):
+                    double_fail[self.current_mission][len(self.players)] and c[False] > 1):
             self.mission[self.current_mission] = False
         else:
             self.mission[self.current_mission] = True
